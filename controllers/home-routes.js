149724d8e8924838e95b3c237f46662da1da8ae9
Nadya <hoperu1986@gmail.com>
2022-07-06 13:37:41 -0400

    homepage with all pets created

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Pet } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage');
+    Pet.findAll({
+        attributes: [
+            'id',
+            'pet_name',
+            'species',
+            'breed'
+        ]
+    })
+  .then((dbPetData) => {
+    const pets = dbPetData.map(pet => pet.get({ plain: true }));
+    res.render('homepage', {pets});
+})
+  .catch(err => {res.status(500).json(err);
+});
 });
 
 module.exports = router;
