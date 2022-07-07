c38e6ea371d395bd8aa1d0b55ac53fd36084c593
poofy <skyang014@gmail.com>
2022-07-06 20:11:18 -0700

    misssed a few files

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,30 +3,31 @@ const sequelize = require('../config/connection');
 const { Pet } = require('../models');
 
 router.get('/', (req, res) => {
-    console.log(req.session);
-    Pet.findAll({
-        attributes: [
-            'id',
-            'pet_name',
-            'species',
-            'breed'
-        ]
+  console.log(req.session);
+  Pet.findAll({
+    attributes: [
+      'id',
+      'pet_name',
+      'species',
+      'breed'
+    ]
+  })
+    .then((dbPetData) => {
+      const pets = dbPetData.map(pet => pet.get({ plain: true }));
+      res.render('homepage', { pets, loggedIn: req.session.loggedIn });
     })
-  .then((dbPetData) => {
-    const pets = dbPetData.map(pet => pet.get({ plain: true }));
-    res.render('homepage', {pets});
-})
-  .catch(err => {res.status(500).json(err);
-});
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-      }
-    
-      res.render('login');
-    });
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
 
 module.exports = router;
